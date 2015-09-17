55004b39437db051810782f6b0ddb4910f680f6b
alybel <al.d.beck@gmail.com>
2015-09-17 21:19:39 +0200

    added on/off swithc

--- app/main/views.py
+++ app/main/views.py
@@ -6,8 +6,14 @@ from flask.ext.login import current_user
 
 @main.route('/')
 def index():
-    sett = GeneralSettings.query.filter_by(fk_user_id=current_user.id).first()
+    sett = None
+    att = 0
+    try:
+        sett = GeneralSettings.query.filter_by(fk_user_id=current_user.id).first()
+        att = sett.onoff
+    except:
+        pass
     form = OnOffForm()
-    form.submit.label.text = "switch off" if sett.onoff == 1 else "switch on"
-    return render_template('index.html', form=form, onoff = sett.onoff)
+    form.submit.label.text = "switch off" if att == 1 else "switch on"
+    return render_template('index.html', form=form, onoff = att)
 
