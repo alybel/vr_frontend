3f4ff1be2765a7f3a3942e1c8a02afa530c08046
matyas <matthias.karacsonyi@gmail.com>
2015-11-13 14:15:40 +0100

    changed wsgi

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,8 @@
 __author__ = 'matyas'
 import os
-import app as Application
-from flask.ext.script import Manager, Shell
+from app import create_app
 
-#app = create_app(os.getenv('FLASK_CONFIG') or 'default')
+app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 
 if __name__ == '__main__':
-    Application.run()
+    app.run()
