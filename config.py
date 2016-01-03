cfd853c05e6323acd5980f3821b505a5632ebc76
matyas <matthias.karacsonyi@gmail.com>
2016-01-03 10:29:41 +0100

    local changes

--- config.py
+++ config.py
@@ -25,16 +25,16 @@ class DevelopmentConfig(Config):
     DEBUG = True
     # mysql://username:password@server/db
     a = os.environ
-    SQLALCHEMY_DATABASE_URI = "............."
+    SQLALCHEMY_DATABASE_URI = "mysql://root:valuereachdb@localhost:3306/valuereach"
 
 
 class TestingConfig(Config):
     TESTING = True
-    SQLALCHEMY_DATABASE_URI = "............."
+    SQLALCHEMY_DATABASE_URI = "mysql://root:valuereachdb@localhost:3306/valuereach"
 
 
 class ProductionConfig(Config):
-    SQLALCHEMY_DATABASE_URI = "........."
+    SQLALCHEMY_DATABASE_URI = "mysql://root:valuereachdb@localhost:3306/valuereach"
  
 
 
